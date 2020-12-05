1cfa6b3aa8a0f7d60eeed2b606ac574c7fc7c8c8
Andrea Bedini <andrea@andreabedini.com>
2020-12-05 10:16:56 +0800

    Brush up

--- site.hs
+++ site.hs
@@ -1,13 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Applicative
-import           Data.Char (toUpper)
-import           Data.List (isSuffixOf)
-import           Data.String
-import           Data.Monoid ((<>))
+import           Data.List             (isSuffixOf)
 import           Hakyll
-import           Hakyll.Core.Compiler.Internal
-import           System.FilePath.Posix  (takeBaseName,takeDirectory,(</>))
-
+import           System.FilePath.Posix (takeBaseName,takeDirectory,(</>))
+import qualified GHC.IO.Encoding as E
 
 --------------------------------------------------------------------------------
 
@@ -31,13 +26,10 @@ cleanIndex url
 
 --------------------------------------------------------------------------------
 
-pages :: IsString a => [a]
-pages = [ "about.markdown"
-        , "academia.markdown"
-        ]
-
 main :: IO ()
-main = hakyll $ do
+main = do
+  E.setLocaleEncoding E.utf8
+  hakyll $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -74,33 +66,19 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= cleanIndexUrls
 
-    match "404.markdown" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/page.html"    defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= cleanIndexUrls
-
-    match (fromList pages) $ do
+    match "*.markdown" $ do
         route     cleanRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/page.html"    defaultContext
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= cleanIndexUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                  listField "posts" postCtx (return posts) <>
-                  constField "title" "Home"                <>
-                  defaultContext
-
-            getResourceBody
-              >>= applyAsTemplate indexCtx
-              >>= loadAndApplyTemplate "templates/default.html" indexCtx
-              >>= cleanIndexUrls
+    match "404.markdown" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/page.html"    defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= cleanIndexUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
@@ -110,4 +88,3 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" <>
     defaultContext
-
